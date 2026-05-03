2e1e2b18398f6f958da19a857f5b6a952dfa6468
WENDER HELTON <wender.helton.wh@gmail.com>
2026-05-03 13:16:12 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "The blog - Este é um blog com Next.js",
+  description: "Essa seria a descrição dessa página.",
+};
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
+  return (
+    <html lang="pt-BR">
+      <body className='qualquer'>
+        <header>
+          <h1>Header</h1>
+        </header>
+
+        <div className="bg-red-500">{children}</div>
+        
+        <footer>
+          <h1>Footer</h1>
+        </footer>
+      </body>
+    </html>
+  );
+}
